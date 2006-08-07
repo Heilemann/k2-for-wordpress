5afde91a84d103c64375afa994ab627380420b2e
ben.sherratt <ben.sherratt@0da3f984-a319-0410-8551-939afe8cdb0b>
2006-08-07 09:38:33 +0000

    Separation of code & content in options/display/form.php. Cleaned up the tabbing and optimised the code slightly.
    Optimised counting statements in page-archives.php - COUNT(1) removes the need to extract actual data from the database, therefore it's faster than COUNT(*).

--- page-archives.php
+++ page-archives.php
@@ -3,13 +3,13 @@
 */ ?>
 
 <?php /* Counts the posts, comments and categories on your blog */
-	$numposts = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->posts WHERE post_status = 'publish'");
+	$numposts = $wpdb->get_var("SELECT COUNT(1) FROM $wpdb->posts WHERE post_status = 'publish'");
 	if (0 < $numposts) $numposts = number_format($numposts); 
 	
-	$numcomms = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->comments WHERE comment_approved = '1'");
+	$numcomms = $wpdb->get_var("SELECT COUNT(1) FROM $wpdb->comments WHERE comment_approved = '1'");
 	if (0 < $numcomms) $numcomms = number_format($numcomms);
 	
-	$numcats = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->categories");
+	$numcats = $wpdb->get_var("SELECT COUNT(1) FROM $wpdb->categories");
 	if (0 < $numcats) $numcats = number_format($numcats);
 ?>
 
