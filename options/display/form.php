07f343167ab7369eb730320e52939965ffbc7086
safirul.alredha <safirul.alredha@0da3f984-a319-0410-8551-939afe8cdb0b>
2006-08-07 11:18:39 +0000

    Is there any reason why the Uninstall update notice is removed?

--- options/display/form.php
+++ options/display/form.php
@@ -55,6 +55,12 @@
 </div>
 <?php } ?>
 
+<?php if (isset($_POST['uninstall'])) : ?>
+	<div class="updated">
+		<p><?php _e('K2 Options have been uninstall','k2_domain'); ?></p>
+	</div>
+<?php endif;?>
+
 <div class="wrap">
 	<h2><?php _e('K2 Options','k2_domain'); ?></h2>
 
