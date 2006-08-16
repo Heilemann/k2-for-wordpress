c448546319e2e31af4ee386e47c5fe653adc4b82
ben.sherratt <ben.sherratt@0da3f984-a319-0410-8551-939afe8cdb0b>
2006-08-16 17:28:01 +0000

    Vanishing code fixes.

--- modules/html.php
+++ modules/html.php
@@ -4,17 +4,17 @@ function html_sidebar_module($args) {
 	extract($args);
 
 	echo($before_module . $before_title . $title . $after_title);
-	echo(stripslashes(sbm_get_option('html')));
+	echo(sbm_get_option('html'));
 	echo($after_module);
 }
 
 function html_sidebar_module_control() {
 	if(isset($_POST['html_module_html'])) {
-		sbm_update_option('html', $_POST['html_module_html']);
+		sbm_update_option('html', stripslashes($_POST['html_module_html']));
 	}
 
 	?>
-		<p><label for="html-module-html">Module's HTML:</label><br /><textarea id="html-module-html" name="html_module_html" rows="6" cols="30"><?php echo(stripslashes(sbm_get_option('html'))); ?></textarea></p>
+		<p><label for="html-module-html">Module's HTML:</label><br /><textarea id="html-module-html" name="html_module_html" rows="6" cols="30"><?php echo(wp_specialchars(htmlspecialchars(stripslashes(sbm_get_option('html')), ENT_QUOTES), 1)); ?></textarea></p>
 	<?php
 }
 
--- modules/text.php
+++ modules/text.php
@@ -4,17 +4,17 @@ function text_sidebar_module($args) {
 	extract($args);
 
 	echo($before_module . $before_title . $title . $after_title);
-	echo('<p>' . nl2br(wp_specialchars(htmlspecialchars(stripslashes(sbm_get_option('text')), ENT_QUOTES), 1)) . '</p>');
+	echo('<p>' . nl2br(wp_specialchars(htmlspecialchars(sbm_get_option('text'), ENT_QUOTES), 1)) . '</p>');
 	echo($after_module);
 }
 
 function text_sidebar_module_control() {
 	if(isset($_POST['text_module_text'])) {
-		sbm_update_option('text', $_POST['text_module_text']);
+		sbm_update_option('text', stripslashes($_POST['text_module_text']));
 	}
 
 	?>
-		<p><label for="text-module-text">Module's text content:</label><br /><textarea id="text-module-text" name="text_module_text" rows="6" cols="30"><?php echo(stripslashes(sbm_get_option('text'))); ?></textarea></p>
+		<p><label for="text-module-text">Module's text content:</label><br /><textarea id="text-module-text" name="text_module_text" rows="6" cols="30"><?php echo(wp_specialchars(htmlspecialchars(sbm_get_option('text'), ENT_QUOTES), 1)); ?></textarea></p>
 	<?php
 }
 
