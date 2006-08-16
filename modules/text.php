82a200e5b8c724ae092c449e93bb90123794f2ac
ben.sherratt <ben.sherratt@0da3f984-a319-0410-8551-939afe8cdb0b>
2006-08-16 17:31:07 +0000

    Slight text module fix.

--- modules/text.php
+++ modules/text.php
@@ -4,17 +4,17 @@ function text_sidebar_module($args) {
 	extract($args);
 
 	echo($before_module . $before_title . $title . $after_title);
-	echo('<p>' . nl2br(wp_specialchars(htmlspecialchars(sbm_get_option('text'), ENT_QUOTES), 1)) . '</p>');
+	echo('<p>' . nl2br(sbm_get_option('text')) . '</p>');
 	echo($after_module);
 }
 
 function text_sidebar_module_control() {
 	if(isset($_POST['text_module_text'])) {
-		sbm_update_option('text', stripslashes($_POST['text_module_text']));
+		sbm_update_option('text', wp_specialchars(htmlspecialchars(stripslashes($_POST['text_module_text']), ENT_QUOTES), 1));
 	}
 
 	?>
-		<p><label for="text-module-text">Module's text content:</label><br /><textarea id="text-module-text" name="text_module_text" rows="6" cols="30"><?php echo(wp_specialchars(htmlspecialchars(sbm_get_option('text'), ENT_QUOTES), 1)); ?></textarea></p>
+		<p><label for="text-module-text">Module's text content:</label><br /><textarea id="text-module-text" name="text_module_text" rows="6" cols="30"><?php echo(sbm_get_option('text')); ?></textarea></p>
 	<?php
 }
 
