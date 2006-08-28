384c93b67cfbc22a061f15a70323033bc3f2ed0a
ben.sherratt <ben.sherratt@0da3f984-a319-0410-8551-939afe8cdb0b>
2006-08-28 15:55:29 +0000

    Nicer BLC options for modules/recent_comments.php

--- modules/recent_comments.php
+++ modules/recent_comments.php
@@ -14,7 +14,7 @@ function recent_comments_sidebar_module($args) {
 	if(function_exists('blc_latest_comments')) {
 		?>
 		<ul>
-			<?php blc_latest_comments(sbm_get_option('num_comments'),'3','false'); ?>
+			<?php blc_latest_comments(sbm_get_option('num_posts'), sbm_get_option('num_comments'), false, "<li class='alternate'>", '</li>', true, 10, sbm_get_option('new_color'), sbm_get_option('old_color')); ?>
 		</ul>
 		<?php
 	} else {
@@ -40,16 +40,41 @@ function recent_comments_sidebar_module($args) {
 }
 
 function recent_comments_sidebar_module_control() {
+	if(isset($_POST['recent_comments_module_num_posts'])) {
+		sbm_update_option('num_comments', $_POST['recent_comments_module_num_posts']);
+	}
+
 	if(isset($_POST['recent_comments_module_num_comments'])) {
 		sbm_update_option('num_comments', $_POST['recent_comments_module_num_comments']);
 	}
 
+	if(isset($_POST['recent_comments_module_new_color'])) {
+		sbm_update_option('new_color', $_POST['recent_comments_module_new_color']);
+	}
+
+	if(isset($_POST['recent_comments_module_old_color'])) {
+		sbm_update_option('old_color', $_POST['recent_comments_module_old_color']);
+	}
+
+	if(function_exists('blc_latest_comments')) {
+	?>
+		<p><label for="recent-comments-module-num-posts"><?php _e('Number of posts:', 'k2_domain'); ?></label> <input id="recent-comments-module-num-posts" name="recent_comments_module_num_posts" type="text" value="<?php echo(sbm_get_option('num_posts')); ?>" size="2" /></p>
+	<?php
+	}
 	?>
 		<p><label for="recent-comments-module-num-comments"><?php _e('Number of comments:', 'k2_domain'); ?></label> <input id="recent-comments-module-num-comments" name="recent_comments_module_num_comments" type="text" value="<?php echo(sbm_get_option('num_comments')); ?>" size="2" /></p>
 	<?php
+
+	if(function_exists('blc_latest_comments')) {
+	?>
+		<p><label for="recent-comments-module-new-color"><?php _e('Newest color:', 'k2_domain'); ?></label> <input id="recent-comments-module-new-color" name="recent_comments_module_new_color" type="text" value="<?php echo(sbm_get_option('new_color')); ?>" size="7" /></p>
+
+		<p><label for="recent-comments-module-old-color"><?php _e('Oldest color:', 'k2_domain'); ?></label> <input id="recent-comments-module-old-color" name="recent_comments_module_old_color" type="text" value="<?php echo(sbm_get_option('old_color')); ?>" size="7" /></p>
+	<?php
+	}
 }
 
-register_sidebar_module('Recent comments module', 'recent_comments_sidebar_module', 'sb-comments' . (function_exists('blc_latest_comments') ? ' sb-comments-blc' : ''), array('num_comments' => 10));
+register_sidebar_module('Recent comments module', 'recent_comments_sidebar_module', 'sb-comments' . (function_exists('blc_latest_comments') ? ' sb-comments-blc' : ''), array('num_posts' => 5, 'num_comments' => 10, 'new_color' => '#444444', 'old_color' => '#cccccc'));
 register_sidebar_module_control('Recent comments module', 'recent_comments_sidebar_module_control');
 
 ?>
