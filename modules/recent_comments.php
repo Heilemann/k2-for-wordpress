f92ed27d714f28605a84264e48229358e2a32b9a
ben.sherratt <ben.sherratt@0da3f984-a319-0410-8551-939afe8cdb0b>
2006-08-30 13:22:51 +0000

    Better recent_comments.php

--- modules/recent_comments.php
+++ modules/recent_comments.php
@@ -14,12 +14,19 @@ function recent_comments_sidebar_module($args) {
 	if(function_exists('blc_latest_comments')) {
 		?>
 		<ul>
-			<?php blc_latest_comments(sbm_get_option('num_posts'), sbm_get_option('num_comments'), false, "<li class='alternate'>", '</li>', true, 10, sbm_get_option('new_color'), sbm_get_option('old_color')); ?>
+			<?php blc_latest_comments(sbm_get_option('num_posts'), sbm_get_option('num_comments'), sbm_get_option('hide_trackbacks'), "<li class='alternate'>", '</li>', true, 10, sbm_get_option('new_color'), sbm_get_option('old_color')); ?>
 		</ul>
 		<?php
 	} else {
 		$num_comments = sbm_get_option('num_comments');
-		$comments = $wpdb->get_results("SELECT comment_ID, comment_post_ID, comment_author, comment_author_url FROM $wpdb->comments WHERE comment_approved = '1' ORDER BY comment_date_gmt DESC LIMIT $num_comments");
+		$sql = "SELECT comment_ID, comment_post_ID, comment_author, comment_author_url FROM $wpdb->comments WHERE comment_approved = '1' ";
+
+		if(sbm_get_option('hide_trackbacks') == true) {
+			$sql .= "AND comment_type = '' ";
+		}
+
+		$sql .= "ORDER BY comment_date_gmt DESC LIMIT $num_comments";
+		$comments = $wpdb->get_results($sql);
 
 		if($comments) {
 		?>
@@ -40,41 +47,41 @@ function recent_comments_sidebar_module($args) {
 }
 
 function recent_comments_sidebar_module_control() {
-	if(isset($_POST['recent_comments_module_num_posts'])) {
-		sbm_update_option('num_comments', $_POST['recent_comments_module_num_posts']);
-	}
+	if(isset($_POST['recent_comments_module'])) {
+		// Trackbacks are a special case
+		if(isset($_POST['recent_comments_module']['hide_trackbacks'])) {
+			sbm_update_option('hide_trackbacks', true);
+			unset($_POST['recent_comments_module']['hide_trackbacks']);
+		} else {
+			sbm_update_option('hide_trackbacks', false);
+		}
 
-	if(isset($_POST['recent_comments_module_num_comments'])) {
-		sbm_update_option('num_comments', $_POST['recent_comments_module_num_comments']);
-	}
-
-	if(isset($_POST['recent_comments_module_new_color'])) {
-		sbm_update_option('new_color', $_POST['recent_comments_module_new_color']);
-	}
-
-	if(isset($_POST['recent_comments_module_old_color'])) {
-		sbm_update_option('old_color', $_POST['recent_comments_module_old_color']);
+		foreach($_POST['recent_comments_module'] as $key => $value) {
+			sbm_update_option($key, $value);
+		}
 	}
 
 	if(function_exists('blc_latest_comments')) {
 	?>
-		<p><label for="recent-comments-module-num-posts"><?php _e('Number of posts:', 'k2_domain'); ?></label> <input id="recent-comments-module-num-posts" name="recent_comments_module_num_posts" type="text" value="<?php echo(sbm_get_option('num_posts')); ?>" size="2" /></p>
+		<p><label for="recent-comments-module-num-posts"><?php _e('Number of posts:', 'k2_domain'); ?></label> <input id="recent-comments-module-num-posts" name="recent_comments_module[num_posts]" type="text" value="<?php echo(sbm_get_option('num_posts')); ?>" size="2" /></p>
 	<?php
 	}
 	?>
-		<p><label for="recent-comments-module-num-comments"><?php _e('Number of comments:', 'k2_domain'); ?></label> <input id="recent-comments-module-num-comments" name="recent_comments_module_num_comments" type="text" value="<?php echo(sbm_get_option('num_comments')); ?>" size="2" /></p>
+		<p><label for="recent-comments-module-num-comments"><?php _e('Number of comments:', 'k2_domain'); ?></label> <input id="recent-comments-module-num-comments" name="recent_comments_module[num_comments]" type="text" value="<?php echo(sbm_get_option('num_comments')); ?>" size="2" /></p>
+
+		<p><label for="recent-comments-module-hide-trackbacks"><?php _e('Hide trackbacks/pingbacks:', 'k2_domain'); ?></label> <input id="recent-comments-module-hide-trackbacks" name="recent_comments_module[hide_trackbacks]" type="checkbox"<?php if(sbm_get_option('hide_trackbacks')) { ?> checked="checked"<?php } ?> /></p>
 	<?php
 
 	if(function_exists('blc_latest_comments')) {
 	?>
-		<p><label for="recent-comments-module-new-color"><?php _e('Newest color:', 'k2_domain'); ?></label> <input id="recent-comments-module-new-color" name="recent_comments_module_new_color" type="text" value="<?php echo(sbm_get_option('new_color')); ?>" size="7" /></p>
+		<p><label for="recent-comments-module-new-color"><?php _e('Newest color:', 'k2_domain'); ?></label> <input id="recent-comments-module-new-color" name="recent_comments_module[new_color]" type="text" value="<?php echo(sbm_get_option('new_color')); ?>" size="7" /></p>
 
-		<p><label for="recent-comments-module-old-color"><?php _e('Oldest color:', 'k2_domain'); ?></label> <input id="recent-comments-module-old-color" name="recent_comments_module_old_color" type="text" value="<?php echo(sbm_get_option('old_color')); ?>" size="7" /></p>
+		<p><label for="recent-comments-module-old-color"><?php _e('Oldest color:', 'k2_domain'); ?></label> <input id="recent-comments-module-old-color" name="recent_comments_module[old_color]" type="text" value="<?php echo(sbm_get_option('old_color')); ?>" size="7" /></p>
 	<?php
 	}
 }
 
-register_sidebar_module('Recent comments module', 'recent_comments_sidebar_module', 'sb-comments' . (function_exists('blc_latest_comments') ? ' sb-comments-blc' : ''), array('num_posts' => 5, 'num_comments' => 10, 'new_color' => '#444444', 'old_color' => '#cccccc'));
+register_sidebar_module('Recent comments module', 'recent_comments_sidebar_module', 'sb-comments' . (function_exists('blc_latest_comments') ? ' sb-comments-blc' : ''), array('num_posts' => 5, 'num_comments' => 10, 'hide_trackbacks' => false, 'new_color' => '#444444', 'old_color' => '#cccccc'));
 register_sidebar_module_control('Recent comments module', 'recent_comments_sidebar_module_control');
 
 ?>
