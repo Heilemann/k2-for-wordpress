7987859edd8176fe5a8ff9bc64cd672498be7806
Heilemann <Heilemann@0da3f984-a319-0410-8551-939afe8cdb0b>
2006-09-05 13:07:40 +0000

    Updated script.aculo.us to v1.6.3 and the Prototype library to 1.5.0rc1.

--- js/slider.js.php
+++ js/slider.js.php
@@ -18,7 +18,10 @@
 	header($LmStr);
 	header('Content-Type: text/javascript; charset: UTF-8');
 ?>
-// Copyright (c) 2005 Marty Haught, Thomas Fuchs 
+
+// script.aculo.us slider.js v1.6.3, Tue Sep 05 12:16:13 CEST 2006
+// See http://script.aculo.us for more info
+
 
 if(!Control) var Control = {};
 Control.Slider = Class.create();
@@ -63,7 +66,12 @@ Control.Slider.prototype = {
     this.alignY = parseInt(this.options.alignY || '0');
     
     this.trackLength = this.maximumOffset() - this.minimumOffset();
-    this.handleLength = this.isVertical() ? this.handles[0].offsetHeight : this.handles[0].offsetWidth;
+
+    this.handleLength = this.isVertical() ? 
+      (this.handles[0].offsetHeight != 0 ? 
+        this.handles[0].offsetHeight : this.handles[0].style.height.replace(/px$/,"")) : 
+      (this.handles[0].offsetWidth != 0 ? this.handles[0].offsetWidth : 
+        this.handles[0].style.width.replace(/px$/,""));
 
     this.active   = false;
     this.dragging = false;
@@ -179,8 +187,11 @@ Control.Slider.prototype = {
     return(this.isVertical() ? this.alignY : this.alignX);
   },
   maximumOffset: function(){
-    return(this.isVertical() ?
-      this.track.offsetHeight - this.alignY : this.track.offsetWidth - this.alignX);
+    return(this.isVertical() ? 
+      (this.track.offsetHeight != 0 ? this.track.offsetHeight :
+        this.track.style.height.replace(/px$/,"")) - this.alignY : 
+      (this.track.offsetWidth != 0 ? this.track.offsetWidth : 
+        this.track.style.width.replace(/px$/,"")) - this.alignY);
   },  
   isVertical:  function(){
     return (this.axis == 'vertical');
