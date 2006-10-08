62703ccf1d83be6d619f5cf1694503a4d1ad728f
safirul.alredha <safirul.alredha@0da3f984-a319-0410-8551-939afe8cdb0b>
2006-10-08 04:09:33 +0000

    Adding back Effect.Phase, a custom effect for scriptaculous.

--- js/effects.js.php
+++ js/effects.js.php
@@ -989,3 +989,22 @@ Element.Methods.visualEffect = function(element, effect, options) {
 };
 
 Element.addMethods();
+
+Effect.PhaseIn = function(element) {
+  element = $(element);
+  new Effect.BlindDown(element, arguments[1] || {});
+  new Effect.Appear(element, arguments[2] || arguments[1] || {});
+}
+
+Effect.PhaseOut = function(element) {
+  element = $(element);
+  new Effect.Fade(element, arguments[1] || {});
+  new Effect.BlindUp(element, arguments[2] || arguments[1] || {});
+}
+
+Effect.Phase = function(element) {
+  element = $(element);
+  if (element.style.display == 'none')
+    new Effect.PhaseIn(element, arguments[1] || {}, arguments[2] || arguments[1] || {});
+  else new Effect.PhaseOut(element, arguments[1] || {}, arguments[2] || arguments[1] || {});
+}
