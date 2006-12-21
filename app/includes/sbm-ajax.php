a5830f38774321a8aedd5a3b170f4f803b75df79
ben.sherratt <ben.sherratt@0da3f984-a319-0410-8551-939afe8cdb0b>
2006-12-21 21:42:27 +0000

    Removed old WP header.

--- app/includes/sbm-ajax.php
+++ app/includes/sbm-ajax.php
@@ -2,7 +2,6 @@
 	require_once('../../../../../wp-blog-header.php');
 	require_once(ABSPATH . 'wp-admin/admin-functions.php');
 	require_once(ABSPATH . 'wp-admin/admin-db.php');
-	require_once(ABSPATH . WPINC . '/registration-functions.php');
 
 	k2sbm::direct_bootstrap();
 ?>
