ac1402315101ec22efd617c425372d1b2e976da1
spikeyslam <spikeyslam@0da3f984-a319-0410-8551-939afe8cdb0b>
2007-01-30 09:07:06 +0000

    Removed search message. Using WP 2.1's the_search_query.

--- searchform.php
+++ searchform.php
@@ -1,13 +1,4 @@
-<?php if (!is_search()) {
-		$search_text = __('search blog archives','k2_domain');
-	} else {
-		$search_text = "$s";
-	}
-?>
-
 <form method="get" id="searchform" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-	<input type="text" id="s" name="s" value="<?php echo wp_specialchars($search_text, 1); ?>" />
+	<input type="text" id="s" name="s" value="<?php the_search_query(); ?>" />
 	<input type="submit" id="searchsubmit" value="<?php _e('go','k2_domain'); ?>" />
 </form>
-
-
