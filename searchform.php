8db492a9cf0a0d48b73d42a1fa9e689dfcaad184
spikeyslam <spikeyslam@0da3f984-a319-0410-8551-939afe8cdb0b>
2007-05-09 16:27:06 +0000

    changed action of searchform to site's home

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form method="get" id="searchform" action="<?php echo attribute_escape($_SERVER['PHP_SELF']); ?>">
+<form method="get" id="searchform" action="<?php bloginfo('url'); ?>">
 	<input type="text" id="s" name="s" value="<?php the_search_query(); ?>" />
 	<input type="submit" id="searchsubmit" value="<?php echo attribute_escape(__('go','k2_domain')); ?>" />
 </form>
