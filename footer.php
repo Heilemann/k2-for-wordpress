ec921a8c5ad6a6a1e9574541584e982fdae82552
Heilemann <Heilemann@0da3f984-a319-0410-8551-939afe8cdb0b>
2007-09-14 18:44:11 +0000

    Fixed the localization problem in #316 and rearranged and updated the footer output.

--- footer.php
+++ footer.php
@@ -5,20 +5,21 @@
 <hr />
 
 <div id="footer">
-	<p><?php printf( __('%1$s is powered by %2$s and %3$s','k2_domain'),
-				get_bloginfo('name'),
-				sprintf('<a href="http://wordpress.org/" title="%1$s">%2$s %3$s</a>',
-					__('Where children sing songs of binary bliss','k2_domain'),
-					__('WordPress','k2_domain'),
-					get_bloginfo('version')
-				),
-				sprintf('<a href="http://getk2.com/" title="%1$s">K2<!--%2$s--></a>',
-					__('Loves you like a kitten.','k2_domain'),
-					get_k2info('version')
-				)
-			); ?></p>
-	<?php if (function_exists('k2_style_info')) { k2_style_info(); } ?>
-	<p><?php printf(__('<a href="%1$s">RSS Entries</a> and <a href="%2$s">RSS Comments</a>','k2_domain'), get_bloginfo('rss2_url'), get_bloginfo('comments_rss2_url')) ?></p>
+
+	<?php if (function_exists('k2_style_info')) { ?><p class="footerstyledwith"><?php _e('Styled with ','k2_domain'); k2_style_info(); ?></p><?php } ?>
+
+	<p class="footerpoweredby"><?php printf( __('Powered by %1$s and %2$s','k2_domain'),
+		sprintf('<a href="http://wordpress.org/">%1$s <!--%2$s--></a>',
+			__('WordPress','k2_domain'),
+			get_bloginfo('version')
+		),
+		sprintf('<a href="http://getk2.com/" title="%1$s">K2<!--%2$s--></a>',
+			__('Loves you like a kitten.','k2_domain'),
+			get_k2info('version')
+		)
+	); ?></p>
+
+	<p class="footerfeedlinks"><?php printf(__('<a href="%1$s">Entries Feed</a> and <a href="%2$s">Comments Feed</a>','k2_domain'), get_bloginfo('rss2_url'), get_bloginfo('comments_rss2_url')) ?></p>
 	<!-- <?php printf(__('%d queries. %.4f seconds.','k2_domain'), $wpdb->num_queries , timer_stop()) ?> -->
 </div>
 
