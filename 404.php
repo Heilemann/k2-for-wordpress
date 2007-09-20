f9e56d8ddf9a4bf31eb2cd172f7bf9df4fc87874
Heilemann <Heilemann@0da3f984-a319-0410-8551-939afe8cdb0b>
2007-09-20 23:22:09 +0000

    Fixed #378 (404 page tweak)

--- 404.php
+++ 404.php
@@ -16,7 +16,7 @@
 				</div>
 
 				<div class="entry-content">
-					<p><?php _e('Oh no! You\'re looking for something which just isn\'t here! Fear not however, errors are to be expected, and luckily there are tools on the sidebar for you to use in your search for what you need.','k2_domain'); ?></p>
+					<p><?php _e('Oh no! You\'re looking for something which just isn\'t here!','k2_domain'); ?></p>
 				</div>
 
 			</div> <!-- .hentry .four04 -->
