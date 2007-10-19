87c209e35110ac284ce304a04db08be73e519607
spikeyslam <spikeyslam@0da3f984-a319-0410-8551-939afe8cdb0b>
2007-10-19 17:52:24 +0000

    Split the metadata localization into parts.

--- theloop.php
+++ theloop.php
@@ -101,16 +101,26 @@
 
 				<div class="entry-meta">
 					<?php
-						printf(	__('<span class="meta-start">Published</span> <span class="entry-author"><span class="meta-prep">by </span>%1$s</span> <span class="entry-date">%2$s</span> <span class="entry-categories"><span class="meta-prep">in </span>%3$s</span><span class="meta-end">.</span>','k2_domain'),
-								'<address class="vcard author"><a href="' . get_author_posts_url(get_the_author_ID()) .'" class="url fn" title="'. sprintf(__('View all posts by %s','k2_domain'), attribute_escape(get_the_author())) .'">' . get_the_author() . '</a></address>',
-								( function_exists('time_since') ?
+						printf(	__('<span class="meta-start">Published</span> %1$s %2$s %3$s<span class="meta-end">.</span>','k2_domain'),
+
+							'<span class="entry-author">' .
+							sprintf(  __('<span class="meta-prep">by</span> %s','k2_domain'),
+								'<address class="vcard author"><a href="' . get_author_posts_url(get_the_author_ID()) .'" class="url fn" title="'. sprintf(__('View all posts by %s','k2_domain'), attribute_escape(get_the_author())) .'">' . get_the_author() . '</a></address>'
+							) . '</span>',
+							
+							'<span class="entry-date">' .
+							( function_exists('time_since') ?
 									sprintf(__('%s ago','k2_domain'),
 										'<abbr class="published" title="' . get_the_time('Y-m-d\TH:i:sO') . '">' . time_since(abs(strtotime($post->post_date_gmt . " GMT")), time()) . '</addr>') :
-									sprintf(__('<span class="meta-prep">on </span>%s','k2_domain'),
+									sprintf(__('<span class="meta-prep">on</span> %s','k2_domain'),
 										'<abbr class="published" title="' . get_the_time('Y-m-d\TH:i:sO') . '">'. get_the_time($dateformat) . '</addr>')
-								),
+							) . '</span>',
+
+							'<span class="entry-categories">' .
+							sprintf( __('<span class="meta-prep">in</span> %s','k2_domain'),
 								k2_nice_category(', ', __(' and ','k2_domain'))
-							); 
+							) . '</span>'
+						);
 					?>
 
 					<?php /* Comments */ comments_popup_link('0&nbsp;<span>'.__('Comments','k2_domain').'</span>', '1&nbsp;<span>'.__('Comment','k2_domain').'</span>', '%&nbsp;<span>'.__('Comments','k2_domain').'</span>', 'commentslink', '<span>'.__('Closed','k2_domain').'</span>'); ?>
