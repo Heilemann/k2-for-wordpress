a8a972fc2c4ed0551db30eb22bd1be24fbf58b95
spikeyslam <spikeyslam@0da3f984-a319-0410-8551-939afe8cdb0b>
2007-10-22 04:57:52 +0000

    Initial SBM appearance fixes for IE7. Forgot to include changes to sbm-direct.

--- app/includes/sbm-direct.php
+++ app/includes/sbm-direct.php
@@ -3,5 +3,13 @@
 	//require_once(ABSPATH . 'wp-admin/admin-functions.php');
 	//require_once(ABSPATH . 'wp-admin/admin-db.php');
 
+
+	if ( !function_exists('selected') ) {
+		function selected( $selected, $current) {
+			if ( $selected == $current)
+				echo ' selected="selected"';
+		}
+	}
+
 	K2SBM::direct_bootstrap();
 ?>
