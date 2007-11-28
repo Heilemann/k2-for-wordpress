53ee19a0f9978ef639a3153135af4b76bf99f4d9
spikeyslam <spikeyslam@0da3f984-a319-0410-8551-939afe8cdb0b>
2007-11-28 18:44:33 +0000

    Updated Feed URLs for flickrRSS module

--- app/modules/flickr.php
+++ app/modules/flickr.php
@@ -3,9 +3,23 @@
 function flickr_sidebar_module($args) {
 	extract($args);
 
+	// Get flickrRSS Settings
+	$type = get_option('flickrRSS_display_type');
+	$tags = trim(get_option('flickrRSS_tags'));
+	$userid = stripslashes(get_option('flickrRSS_flickrid'));
+
+	// Generate Feed Link
+	if ($type == 'public') {
+		$rss_url = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=' . $tags . '&amp;format=rss_200';
+	} elseif ($type == 'user') {
+		$rss_url = 'http://api.flickr.com/services/feeds/photos_public.gne?id=' . $userid . '&amp;tags=' . $tags . '&amp;format=rss_200';
+	} elseif ($type == 'group') {
+		$rss_url = 'http://api.flickr.com/services/feeds/groups_pool.gne?id=' . $userid . '&amp;format=rss_200';
+	}
+
 	echo($before_module . $before_title . $title . $after_title);
 	?>
-		<a href="http://flickr.com/services/feeds/photos_public.gne?id=<?php echo get_option('flickrRSS_flickrid'); ?>&amp;format=rss_200" title="<?php _e('RSS Feed for flickr','k2_domain'); ?>" class="feedlink"><span><?php _e('RSS','k2_domain'); ?></span></a>
+		<a href="<?php echo $rss_url; ?>" title="<?php _e('RSS Feed for flickr','k2_domain'); ?>" class="feedlink"><span><?php _e('RSS','k2_domain'); ?></span></a>
 
 		<div>
 			<?php get_flickrRSS(); ?>
