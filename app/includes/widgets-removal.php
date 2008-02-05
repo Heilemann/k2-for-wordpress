e1afbd46d03008f1b8ea271c68f5b74977f10bc0
spikeyslam <spikeyslam@0da3f984-a319-0410-8551-939afe8cdb0b>
2008-02-05 23:50:04 +0000

    WP 2.5: Don't disable widgets in Dashboard.

--- app/includes/widgets-removal.php
+++ app/includes/widgets-removal.php
@@ -1,5 +1,19 @@
 <?php
-if (get_option('k2sidebarmanager') == '1') {
-	remove_action('plugins_loaded', 'wp_maybe_load_widgets', 0);
+if ( get_option('k2sidebarmanager') == '1' ) {
+	$disable_widgets = true;
+
+	// WP 2.4+
+	if ( version_compare( floatval( $wp_version ), 2.4, '>=' ) ) {
+		if ( is_admin() ) {
+		 	if ( basename($_SERVER['SCRIPT_FILENAME']) == 'index.php'
+				or basename($_SERVER['SCRIPT_FILENAME']) == 'index-extra.php' ) {
+				$disable_widgets = false;
+			}
+		}
+	}
+
+	if ( $disable_widgets ) {
+		remove_action('plugins_loaded', 'wp_maybe_load_widgets', 0);
+	}
 }
 ?>
