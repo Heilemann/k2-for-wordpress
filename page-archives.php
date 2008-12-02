7106efaf3f8c3e4fd840b4ebf8b26bd814d25417
spikeyslam <spikeyslam@0da3f984-a319-0410-8551-939afe8cdb0b>
2008-12-02 19:11:26 +0000

    Fixed header apperance

--- page-archives.php
+++ page-archives.php
@@ -27,8 +27,10 @@
 
 			<div id="post-<?php the_ID(); ?>" class="<?php k2_post_class(); ?>">
 
-				<div class="page-head">
-					<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title='<?php printf( __('Permanent Link to "%s"','k2_domain'), wp_specialchars(strip_tags(the_title('', '', false)),1) ); ?>'><?php the_title(); ?></a></h2>
+				<div class="entry-head">
+					<h1 class="entry-title">
+						<a href="<?php the_permalink(); ?>" rel="bookmark" title='<?php printf( __('Permanent Link to "%s"','k2_domain'), wp_specialchars(strip_tags(the_title('', '', false)),1) ); ?>'><?php the_title(); ?></a>
+					</h1>
 					<?php edit_post_link(__('Edit','k2_domain'), '<span class="entry-edit">','</span>'); ?>
 				</div>
 
