6c7b2574b081efad27708a8ad7c9e196c64ce4a2
Heilemann <Heilemann@0da3f984-a319-0410-8551-939afe8cdb0b>
2009-12-13 09:22:20 +0000

    This belongs with the last commit.

--- js/k2.options.js
+++ js/k2.options.js
@@ -7,27 +7,9 @@ function confirmDefaults() {
 }
 
 jQuery(document).ready(function(){
-	jQuery('.advanced-option').addClass('hidden');
-
-	jQuery('#advanced-btn').toggle(
-		function () {
-			jQuery(this).val('Hide Advanced Options');
-			jQuery('.advanced-option').removeClass('hidden');
-		},
-		function () {
-			jQuery(this).val('Show Advanced Options');
-			jQuery('.advanced-option').addClass('hidden');
-		}
-	);
 
 	jQuery('#k2-styles').sortable({
 		items: 'tbody tr'
 	});
 
-/*
-	jQuery('#k2-options').submit(function(){
-		if (jQuery('#k2_ajax_complete_cp').length)
-			jQuery('#k2_ajax_complete_cp').val(textareaid.getCode()).removeAttr('disabled');
-	});
-*/
 });
