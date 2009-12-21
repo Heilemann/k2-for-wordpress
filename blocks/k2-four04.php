51f78b6cfd08d9cba15b0d5fa524f852153ab47b
eric.marden <eric.marden@0da3f984-a319-0410-8551-939afe8cdb0b>
2009-12-21 09:46:41 +0000

    MOD: Tweaks to the 404 template

--- blocks/k2-four04.php
+++ blocks/k2-four04.php
@@ -5,7 +5,7 @@
 	</div>
 
 	<div class="entry-content">
-		<p><?php _e('Oh no! You\'re looking for something which just isn\'t here! Fear not however, errors are to be expected, and luckily there are tools on the sidebar for you to use in your search for what you need, or you can browse some of the recent posts.','k2_domain'); ?></p>
+		<p><?php _e('Oh no! You\'re looking for something which just isn\'t here! Fear not however, errors are to be expected. Lucky for you, there are tools in the sidebar for you to use in your search for what you need, or you can browse the most recent posts, listed below.','k2_domain'); ?></p>
 		
 		<h4>Most Recent Posts:</h4>
 	    <ul>
@@ -15,7 +15,7 @@
 	    <li><a href="<?php echo $p->guid; ?>"><?php echo $p->post_title; ?></a></li>
 	 <?php endforeach; ?>
 	    </ul>
-	 
+	
 	</div>
 
 </div><!-- .hentry .four04 -->
