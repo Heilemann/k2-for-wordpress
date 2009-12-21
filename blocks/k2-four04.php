370f30790053244489ab5a159ff8e75be9fd92fe
eric.marden <eric.marden@0da3f984-a319-0410-8551-939afe8cdb0b>
2009-12-21 09:38:03 +0000

    MOD: added latest 5 posts to the 404 template. See #773

--- blocks/k2-four04.php
+++ blocks/k2-four04.php
@@ -5,7 +5,17 @@
 	</div>
 
 	<div class="entry-content">
-		<p><?php _e('Oh no! You\'re looking for something which just isn\'t here! Fear not however, errors are to be expected, and luckily there are tools on the sidebar for you to use in your search for what you need.','k2_domain'); ?></p>
+		<p><?php _e('Oh no! You\'re looking for something which just isn\'t here! Fear not however, errors are to be expected, and luckily there are tools on the sidebar for you to use in your search for what you need, or you can browse some of the recent posts.','k2_domain'); ?></p>
+		
+		<h4>Most Recent Posts:</h4>
+	    <ul>
+	 <?php
+	 $getposts = get_posts();
+	 foreach( $getposts as $p ) : ?>
+	    <li><a href="<?php echo $p->guid; ?>"><?php echo $p->post_title; ?></a></li>
+	 <?php endforeach; ?>
+	    </ul>
+	 
 	</div>
 
 </div><!-- .hentry .four04 -->
