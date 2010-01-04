60e969ae6ce005196a8f096d03657cb60b0f73c9
safirulalredha <safirulalredha@0da3f984-a319-0410-8551-939afe8cdb0b>
2010-01-04 04:41:41 +0000

    "Most Recent Posts:" not localized. Fixes issue #883.

--- blocks/k2-404.php
+++ blocks/k2-404.php
@@ -7,7 +7,7 @@
 	<div class="entry-content">
 		<p><?php _e('Oh no! You\'re looking for something which just isn\'t here! Fear not however, errors are to be expected. Lucky for you, there are tools in the sidebar for you to use in your search for what you need, or you can browse the most recent posts, listed below.', 'k2'); ?></p>
 		
-		<h4>Most Recent Posts:</h4>
+		<h4><?php _e('Most Recent Posts:', 'k2'); ?></h4>
 	    <ul>
 	 <?php
 	 $getposts = get_posts();
