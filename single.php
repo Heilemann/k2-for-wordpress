6cb158d857df694cbc7bae5e219d6848ac37cf85
heilemann <heilemann@0da3f984-a319-0410-8551-939afe8cdb0b>
2010-03-18 20:19:23 +0000

    Minor fix.

--- single.php
+++ single.php
@@ -88,4 +88,11 @@
 
 </div><!-- .wrapper -->
 
+<?php
+	/* Initialize Rolling Archives if needed */
+	if ( defined('DOING_AJAX') and true == DOING_AJAX ) {
+		add_action( 'k2_dynamic_content', array('K2', 'setup_rolling_archives') );
+	}
+?>
+
 <?php get_footer(); ?>
