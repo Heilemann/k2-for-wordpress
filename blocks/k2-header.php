49d3fc05788762da723e6db358f19811bd147ba8
spikeyslam <spikeyslam@0da3f984-a319-0410-8551-939afe8cdb0b>
2010-05-30 19:01:25 +0000

    fixed compatibility with WP < 3.0. re-added accesskey attribute

--- blocks/k2-header.php
+++ blocks/k2-header.php
@@ -14,7 +14,7 @@ $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div';
 
 <<?php echo $heading_tag; ?> id="site-title">
 <span>
-	<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+	<a href="<?php bloginfo( 'url' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" accesskey="1"><?php bloginfo( 'name' ); ?></a>
 </span>
 </<?php echo $heading_tag; ?>>
 <div id="site-description"><?php bloginfo( 'description' ); ?></div>
