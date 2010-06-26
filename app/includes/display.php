856119e53b4d77e46c3ee88305d7c1b6a2d44969
Heilemann <Heilemann@0da3f984-a319-0410-8551-939afe8cdb0b>
2010-06-26 07:38:06 +0000

    Added a few more YouTube sizes for automatic resizing.

--- app/includes/display.php
+++ app/includes/display.php
@@ -97,6 +97,16 @@ add_filter('wp_page_menu', 'k2_page_menu_filter');
  */
 function k2_resize_embeds( $content ) {
 	return str_replace( 'width="480" height="295"></embed>', 'width="500" height="307"></embed>', $content );
+	return str_replace( 'width="480" height="385"></embed>', 'width="500" height="400"></embed>', $content );
+	return str_replace( 'width="560" height="340"></embed>', 'width="500" height="307"></embed>', $content );
+/*
+	return str_replace( 'width="425" height="344"></embed>', 'width="500" height="405"></embed>', $content );
+	return str_replace( 'width="640" height="385"></embed>', 'width="500" height="307"></embed>', $content );
+	return str_replace( 'width="640" height="505"></embed>', 'width="500" height="307"></embed>', $content );
+	return str_replace( 'width="853" height="505"></embed>', 'width="500" height="307"></embed>', $content );
+	return str_replace( 'width="960" height="745"></embed>', 'width="500" height="307"></embed>', $content );
+	return str_replace( 'width="1280" height="745"></embed>', 'width="500" height="307"></embed>', $content );
+*/
 }
 
 add_filter( 'the_content',	'k2_resize_embeds' );
