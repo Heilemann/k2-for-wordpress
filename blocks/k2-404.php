011802aba66070354db01093a13dc09fb3320ba6
safirulalredha <safirulalredha@0da3f984-a319-0410-8551-939afe8cdb0b>
2010-07-03 05:17:44 +0000

    Recent Post doesn't return correct URL in 404 template. Fixes #1021.

--- blocks/k2-404.php
+++ blocks/k2-404.php
@@ -16,14 +16,9 @@
 		<p><?php _e('Oh no! You&#8216;re looking for something which just isn&#8216;t here! Fear not however, errors are to be expected. Lucky for you, there are tools in the sidebar for you to use in your search for what you need, or you can browse the most recent posts, listed below.', 'k2'); ?></p>
 
 		<h4><?php _e('Most Recent Posts:', 'k2'); ?></h4>
-	    <ul>
-	 <?php
-	 $getposts = get_posts();
-	 foreach( $getposts as $p ) : ?>
-	    <li><a href="<?php echo $p->guid; ?>"><?php echo $p->post_title; ?></a></li>
-	 <?php endforeach; ?>
-	    </ul>
-
+		<ul>
+			<?php wp_get_archives(array('type' => 'postbypost', 'limit' => '5')); ?>
+		</ul>
 	</div>
 
 </div> <!-- .hentry .four04 -->
